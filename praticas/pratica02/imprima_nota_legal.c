6d43d59c645b264e8774878283b33c19ec9796b5
emillesb <emillesbdev@gmail.com>
2024-08-22 19:33:43 +0000

    retirando o /t da nota legal

--- praticas/pratica02/imprima_nota_legal.c
+++ praticas/pratica02/imprima_nota_legal.c
@@ -5,10 +5,10 @@ int main(){
   printf("%25s\n", "N O T A   L E G A L");
   printf("%s\n", "==============================");
   printf("%25s\n", "Produto         Qtd Valor Unit");
-  printf("%s\t%7.3i\t%10.2f\n", "Camiseta", 002, 39.99);
-  printf("%s\t%11.3i\t%10.2f\n", "Calca", 001, 89.90);
-  printf("%s\t%7.3i\t%10.2f\n", "Meia Social", 003, 19.99 );
+  printf("%s %10.3i %10.2f\n", "Camiseta", 002, 39.99);
+  printf("%s %13.3i %10.2f\n", "Calca", 001, 89.90);
+  printf("%s %7.3i %10.2f\n", "Meia Social", 003, 19.99 );
    printf("%s\n", "==============================");
-   printf("%s\t%22.2f\n", "Total:", 229.85);
+   printf("%s %23.2f\n", "Total:", 229.85);
   return 0;
 }
