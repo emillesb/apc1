f2025f9c5ed386a7ef7c899fdb0d2919b941b2cf
Emille Brault <emillesbdev@gmail.com>
2024-10-16 10:47:01 -0300

    aula07 comando for

--- aulas/aula07/comando_for.c
+++ aulas/aula07/comando_for.c
@@ -8,7 +8,30 @@ int deu_certo = scanf("%i", &numero);
 
 if (numero > 0 && numero < 11){
     printf("Tabuada de %i\n", numero);
-    printf("1 x %i = %i\n", numero, 1 * numero);
+    for(int i=0; i<10; i++){
+        //printf("%i", i+1); Assim soma de 1+ comecando do 1
+        printf("%i x %i = %i\n", i+1, numero, (i+1)*numero); 
+    }
+    for(int i=10; i>0; i--){
+        printf("%i x %i = %i\n", i, numero, i*numero);
+    }
+    
+//laco infinito  for(int i=10; i>0; i--){
+       // printf("%i x %i = %i\n", i, numero, i*numero);
+
+for(int i=0; i<10; i++){
+    for(int j=0; j<10; j++) {
+        printf("(%i, %i), ", i, j); //aqui sao pontos cartesianos
+    }
+}
+    
+for (;;){
+    printf("Neverending STOOOOOOOOOOORYYYY\n");
+}
+
+
+
+    /*printf("1 x %i = %i\n", numero, 1 * numero);
     printf("2 x %i = %i\n", numero, 2 * numero);
     printf("3 x %i = %i\n", numero, 3 * numero);
     printf("4 x %i = %i\n", numero, 4 * numero);
@@ -17,7 +40,7 @@ if (numero > 0 && numero < 11){
     printf("7 x %i = %i\n", numero, 7 * numero);
     printf("8 x %i = %i\n", numero, 8 * numero);
     printf("9 x %i = %i\n", numero, 9 * numero);
-    printf("10 x %i = %i\n", numero, 10 * numero);
+    printf("10 x %i = %i\n", numero, 10 * numero); */
 } else {
     printf("Esse numero eh invalido, tente novamente mais tarde!");
 } 
